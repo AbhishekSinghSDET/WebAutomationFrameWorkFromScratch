2f58f0ec09e65e7e9db8995ff80af09f45075ce9
abhi2560 <abhisingh2560@gmail.com>
2024-07-31 10:58:45 +0530

    Hybrid Framework

--- src/main/java/com/testing/driver/DriverManager.java
+++ src/main/java/com/testing/driver/DriverManager.java
@@ -1,5 +1,6 @@
 package com.testing.driver;
 
+import com.testing.utils.PropertyReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
